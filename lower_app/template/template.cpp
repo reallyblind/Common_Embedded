0f36b31819a4b3a97ec6046bf10ae5ddd6b34923
reallyblind <101650178+reallyblind@users.noreply.github.com>
2024-09-12 10:06:57 +0800

    Update template.cpp

--- lower_app/template/template.cpp
+++ lower_app/template/template.cpp
@@ -1,12 +1,3 @@
-/*
- * File      : template.cpp
- * template file
- * COPYRIGHT (C) 2020, zc
- *
- * Change Logs:
- * Date           Author       Notes
- * 2020-5-4      zc           the first version
- */
 
 /**
  * @addtogroup IMX6ULL
